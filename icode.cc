83f9badd93acb2548c23cb7e390648c2552b6052
Jay Shoumaker <jay.w.shoumaker@ou.edu>
2021-11-12 22:12:05 +0000

    Implemented steps 4, 5, & 6

--- icode.cc
+++ icode.cc
@@ -131,6 +131,12 @@ int run (itab_t * itab, char * stack, char * static_mem)
           *dst = *src;
         }
         // TASK: Complete case for DTYPE_FLOAT
+	if (op->addr2 == DTYPE_FLOAT)
+        {
+          float * src = (float*)(stack + op->addr3);
+          float * dst = (float*)(stack + op->addr1);
+          *dst = *src;
+        }
         break;
       case OP_LOADCST:
         if (op->addr2 == DTYPE_INT)
@@ -140,6 +146,12 @@ int run (itab_t * itab, char * stack, char * static_mem)
           *dst = *src;
         }
         // TASK: Complete case for DTYPE_FLOAT
+	if (op->addr2 == DTYPE_FLOAT)
+        {
+          float * src = (float*)(static_mem + op->addr3);
+          float * dst = (float*)(stack + op->addr1);
+          *dst = *src;
+        }
         break;
       case OP_STORE:
         if (op->addr2 == DTYPE_INT)
@@ -149,6 +161,12 @@ int run (itab_t * itab, char * stack, char * static_mem)
           *dst = *src;
         }
         // TASK: Complete case for DTYPE_FLOAT
+	if (op->addr2 == DTYPE_FLOAT)
+        {
+          float * src = (float*)(stack + op->addr3);
+          float * dst = (float*)(stack + op->addr1);
+          *dst = *src;
+        }
         break;
       case OP_ADD:
         {
